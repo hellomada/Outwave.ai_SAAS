3453d3977b1851f9ce90382b1cefbb701a0bd269
hellomada <153454965+hellomada@users.noreply.github.com>
2025-08-17 12:14:28 -0400

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Dashboard</h1>
+      <p>This is where email sending will happen.</p>
+    </div>
+  );
+}
