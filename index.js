ca87a7a3b927adad71ee1effeaa76a7c6c062654
hellomada <153454965+hellomada@users.noreply.github.com>
2025-08-17 12:14:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Outwave Frontend Test</h1>
+      <a href="http://localhost:8080/auth/google">
+        Connect Gmail
+      </a>
+    </div>
+  );
+}
